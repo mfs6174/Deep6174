562bba11f37c9aad4f96d7ef2194a28c972a832b
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-09-21 17:46:13 -0700

    output dir in continue

--- continue-train.py
+++ continue-train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
-# File: continue_train.py
-# Date: Thu Sep 18 10:09:22 2014 -0700
+# File: continue-train.py
+# Date: Sun Sep 21 17:46:11 2014 -0700
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 from network_runner import build_nn_with_params
@@ -11,8 +11,8 @@ import gzip
 import sys
 import operator
 
-if len(sys.argv) != 3:
-    print "Usage: {0} <model file to continue on> dataset.pkl.gz".format(sys.argv[0])
+if len(sys.argv) < 3:
+    print "Usage: {0} <model file to continue on> dataset.pkl.gz [output directory]".format(sys.argv[0])
     sys.exit()
 
 # get the network from saved params
@@ -26,6 +26,9 @@ input_size = nn.input_shape
 load_all = reduce(operator.mul, input_size[1:]) < 100 ** 2
 
 dataset = sys.argv[2]
-nn.work(init_learning_rate=0.01,
-        dataset_file=dataset,
-        load_all_data=load_all)
+
+if len(sys.argv) == 4:
+    output_directory = sys.argv[3]
+else:
+    output_directory = dataset + '-output-continue'
+nn.work(0.01, dataset, load_all, output_directory)
