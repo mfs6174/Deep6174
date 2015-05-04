6ddda4bde4e98e20541ae808bf17bea2f0be34e1
Xi-Jin Zhang <zhangxijin91@gmail.com>
2015-05-05 00:32:44 +0800

    add output dir, and let the background be black

--- labeling-image.py
+++ labeling-image.py
@@ -8,15 +8,22 @@ import numpy as np
 import sys
 import cv2
 import gzip
+import cPickle as pickle
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: {} <model> <picture>".format(sys.argv[0]))
+if len(sys.argv) <3:
+    sys.exit("Usage: {} <model> <picture> <output-dir>".format(sys.argv[0]))
 
 from network_runner import get_nn
 
 params_file = sys.argv[1]
 img_file = sys.argv[2]
 nn = get_nn(params_file, 1)
+output_dir = (sys.argv[3] if len(sys.argv)==4 else "./predicted/")
+try:
+    os.mkdir(output_dir)
+except:
+    pass
+assert os.path.isdir(output_dir), "cannot create directory " + output_dir
 
 pred = nn.predict_whole_img(img_file,101)
 to_show = (pred*255.0).astype('uint8')
@@ -24,9 +31,9 @@ label = np.zeros_like(to_show)
 indic = pred.argmax(-1)
 for i in range(label.shape[0]):
     for j in range(label.shape[1]):
-        label[i,j,indic[i,j]] = 255
-cv2.imwrite(img_name+'_predicted.png',to_show)
-cv2.imwrite(img_name+'_labeled.png',label)
-fout = gzip.open(img_name+'_predicted.pkl.gz', 'wb')
+        label[i,j,indic[i,j]] = (255 if indic[i,j] != 2 else 0)
+cv2.imwrite(output_dir+'/'+img_name+'_predicted.png',to_show)
+cv2.imwrite(output_dir+'/'+img_name+'_labeled.png',label)
+fout = gzip.open(output_dir+'/'+img_name+'_predicted.pkl.gz', 'wb')
 pickle.dump(pred, fout, -1)
 fout.close()
